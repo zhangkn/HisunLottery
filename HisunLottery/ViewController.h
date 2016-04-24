d0145a052bc31dc89bca5c5066da35448b4d8d16
Kevin zhang <929118967@qq.com>
2016-04-24 15:25:35 +0800

    Initial Commit

---
+++ HisunLottery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HisunLottery
+//
+//  Created by devzkn on 4/24/16.
+//  Copyright © 2016 hisun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
